d3d64306b130b5e4b037eed6083dc2d874ec21ce
sopeanuts <35119432+sopeanuts@users.noreply.github.com>
2022-02-19 17:37:11 -0700

    Update Assignment_1.java

--- Assignment_1.java
+++ Assignment_1.java
@@ -1,9 +1,11 @@
 
 /**
- * Assignment_1 - Write a description here.
+ * Assignment_1 – Scooter Rental Program - Provides information on the rental services which includes prices,minimum rent and rental rates.
+ *                                         Allows the customer to pick their preferences on rates and have it calculated automatically.
+ *                                         Prints out a receipt on their purchase after personal information has been inputted and payment is completed.
  * 
  * @author David Doan 
- * @version (a version number or a date)
+ * @version (February 20, 2022)
  */
 // Standard import for the Scanner class
 import java.util.*;
@@ -13,7 +15,7 @@ public class Assignment_1
     {
         // Create a Scanner object attached to the keyboard
         Scanner in = new Scanner (System.in);
-        //Opening Message
+        //Opening Message that provides prices and rates
         System.out.println("Welcome to Scoot With Us Rentals");
         System.out.println("Our scooter rental rates are as follows:");
         System.out.println("Daily Rate (8 hours max): $50.00");
@@ -22,22 +24,25 @@ public class Assignment_1
         System.out.println();
         System.out.println("Choose Rental Type (D - Daily or H - Hourly): ");
         String RentalType = in.nextLine().toUpperCase();
-        //Rental type calculations
+        
+        //Rental type calculations based on choices inputted
         if (RentalType.equals("D")){
         
         }else
         System.out.println("Your charge is " + " for" + " hours");
         System.out.println("Please confirm if you want to proceed, Y or N: ");
         String decision = in.nextLine().toUpperCase();
-        //Functions and receipt if recipient chooses "N"(No)
+        
+        //Output message if recipient chooses "N"(No)
         if (decision.equals("N")){
         System.out.println("Transaction is cancelled");
         System.out.println("              /");
         System.out.println("             o--o");
         System.out.println("Scoot With Us Rentals");
         System.out.println("See you next time");
-        //Recipient chooses to proceed and is prompted to enter in personal information
-    }else{
+        
+        //Recipient chooses to proceed and is prompted to enter in personal information for payment
+        }else
         System.out.println("Enter scooter code:");
         String ScooterCode = in.nextLine();
         System.out.println("Enter payment information");
@@ -52,7 +57,8 @@ public class Assignment_1
         System.out.println("Press Enter to unlock and start:");
         in.nextLine();
         System.out.println("Ride safely!");
-        //Receipt output
+        
+        //Receipt output and display
         System.out.println("------------------------------");
         System.out.println("+          Receipt           +");
         System.out.println("------------------------------");
@@ -69,7 +75,6 @@ public class Assignment_1
         System.out.println();
         System.out.println();
         System.out.println("Thank you for renting from us!");
-    }
     }
 }
 
